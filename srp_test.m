08a35455de6edebf5788d74523002c22cdc16cbf
0x00-pl <0x00.pl@gmail.com>
2018-07-17 23:00:26 +0800

    add new data

--- srp_test.m
+++ srp_test.m
@@ -8,14 +8,27 @@ wlen = 4096;
 % ch2R = delayseq(ref, 0.06928*2/340, fs);
 % ch0R = delayseq(ref, 0.06928/340, fs);
 
- ch0L = fread(fopen('./ch0L.pcm', 'r'), inf, 'int32');
- ch0R = fread(fopen('./ch0R.pcm', 'r'), inf, 'int32');
- ch1L = fread(fopen('./ch1L.pcm', 'r'), inf, 'int32');
- ch1R = fread(fopen('./ch1R.pcm', 'r'), inf, 'int32');
- ch2L = fread(fopen('./ch2L.pcm', 'r'), inf, 'int32');
- ch2R = fread(fopen('./ch2R.pcm', 'r'), inf, 'int32');
+addpath('./direc_4/')
+
+ ch0L = fread(fopen('ch0L.pcm', 'r'), inf, 'int32');
+ ch0R = fread(fopen('ch0R.pcm', 'r'), inf, 'int32');
+ ch1L = fread(fopen('ch1L.pcm', 'r'), inf, 'int32');
+ ch1R = fread(fopen('ch1R.pcm', 'r'), inf, 'int32');
+ ch2L = fread(fopen('ch2L.pcm', 'r'), inf, 'int32');
+ ch2R = fread(fopen('ch2R.pcm', 'r'), inf, 'int32');
  fs = 44100;
  
+ size_min = min([...
+     size(ch0L,1) size(ch0R,1) ...
+     size(ch1L,1) size(ch1R,1) ...
+     size(ch2L,1) size(ch2R,1)]);
+ 
+ ch0L = ch0L(1:size_min);
+ ch0R = ch0R(1:size_min);
+ ch1L = ch1L(1:size_min);
+ ch1R = ch1R(1:size_min);
+ ch2L = ch2L(1:size_min);
+ ch2R = ch2R(1:size_min);
 
  
  
